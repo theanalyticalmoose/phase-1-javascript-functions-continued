caaf578a7efaaca660423bdba751ebc174d06285
Rachael <rachael.brady@theanalyticalmoose.com>
2023-09-04 18:27:25 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+// saturdayFun Function
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // mondayWork Function Expression
+  const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // wrapAdjective Function
+  function wrapAdjective(wrapper = '*') {
+    return function(adjective = '') {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    }
+  }
+  
+  // Export these functions if you are using modules
+  // module.exports = {
+  //   saturdayFun,
+  //   mondayWork,
+  //   wrapAdjective
+  // }
+  
